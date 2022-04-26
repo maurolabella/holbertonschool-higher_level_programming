3cd701b6382b5f2912ab90a3b687e326b1bf431e
maurolabella <maurolabella@yahoo.com>
2022-04-25 23:07:11 -0300

    a

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write("and that piece or art is useful - Dora Korpar, \
+sys.stderr.write("and that piece or art is useful - Dora Korpar, \
 2015-10-19\n")
 sys.exit(1)
 
