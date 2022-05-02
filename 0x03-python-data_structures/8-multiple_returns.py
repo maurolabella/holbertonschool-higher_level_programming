7c68b5c6addbb6e69de1bd877b092768e4084b8c
maurolabella <maurolabella@yahoo.com>
2022-05-02 17:00:30 -0300

    a

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if sentence[0]:
+        first = sentence[0]
+    else:
+        first = None
+    res = (length, first)
+    return(res)
