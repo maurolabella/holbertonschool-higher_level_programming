2b065da1e0376c755f29af070dff9c839b4157e7
maurolabella <maurolabella@yahoo.com>
2022-05-02 15:43:00 -0300

    careful with tabs

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-	# m=len(matrix)
-	# n=len(matrix[0])
-	# for i in range(0,m):
-	# 	for j in range(0,n):
-	# 		print("{:d}".format(matrix[i][j]),end="")
-	# 		if(j!=(n-1)):
-	# 			print(" ",end="")
-	# 	print("$")
-	for row in matrix:
-		for elem in row:
-			print("{:d}".format(elem), end="")
-			if (row.index(elem) + 1) < len(row):
-				print(" ", end="")
-		print("\n", end="")
+    # m=len(matrix)
+    # n=len(matrix[0])
+    # for i in range(0,m):
+    # 	for j in range(0,n):
+    # 		print("{:d}".format(matrix[i][j]),end="")
+    # 		if(j!=(n-1)):
+    # 			print(" ",end="")
+    # 	print("$")
+    for row in matrix:
+        for elem in row:
+            print("{:d}".format(elem), end="")
+            if (row.index(elem) + 1) < len(row):
+                print(" ", end="")
+        print("\n", end="")
