6b32002a253bdf9c7c5039e586c7f36d5a138e27
maurolabella <maurolabella@yahoo.com>
2022-05-02 15:34:16 -0300

    a

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+print_matrix_integer = __import__(
+    '6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [1, 2, 3, 5],
+    [4, 5, 6, 7],
+    [7, 8, 9, 8]
 ]
+
 
 print_matrix_integer(matrix)
 print("--")
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    # print(f"{len(matrix)}")
-    # print(f"{len(matrix[0])}")
-	m=len(matrix)
-	n=len(matrix[0])
-	for i in range(0,m):
-		for j in range(0,n):
-			print("{:d}".format(matrix[i][j]),end="")
-			if(j!=(n-1)):
-				print(" ",end="")
-		print("$")
+	# m=len(matrix)
+	# n=len(matrix[0])
+	# for i in range(0,m):
+	# 	for j in range(0,n):
+	# 		print("{:d}".format(matrix[i][j]),end="")
+	# 		if(j!=(n-1)):
+	# 			print(" ",end="")
+	# 	print("$")
+	for row in matrix:
+		for elem in row:
+			print("{:d}".format(elem), end="")
+			if (row.index(elem) + 1) < len(row):
+				print(" ", end="")
+		print("\n", end="")
