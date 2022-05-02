bf61c5b67814c271c85ae8688e4befc925d15890
maurolabella <maurolabella@yahoo.com>
2022-05-02 15:41:29 -0300

    careful with cat

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -3,9 +3,9 @@ print_matrix_integer = __import__(
     '6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3, 5],
-    [4, 5, 6, 7],
-    [7, 8, 9, 8]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ]
 
 
