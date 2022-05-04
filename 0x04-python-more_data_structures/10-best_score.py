57bec4a118ce96f4a77f582d4f427e61ac557e4f
maurolabella <maurolabella@yahoo.com>
2022-05-04 17:11:12 -0300

    a

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,8 @@ def best_score(a_dictionary):
         return None
     keys = list(a_dictionary)
     max = a_dictionary[keys[0]]
+    res = keys[0]
     for key in keys:
         if a_dictionary[key] > max:
-            max = a_dictionary[key]
-    return max
+            res = key
+    return res
