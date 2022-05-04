789a7f6b2f307fb79fb59c1a39de083fbe938b04
maurolabella <maurolabella@yahoo.com>
2022-05-04 14:32:35 -0300

    a

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    tmp = [element for element 
-		in set_1 for x in set_2 if element == x]
+    tmp = [element for element
+           in set_1 for x in set_2 if element == x]
     return tmp
