15f1edededfd06236c88c6c350f5e1e0e1e42d72
maurolabella <maurolabella@yahoo.com>
2022-05-04 13:27:00 -0300

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     col_numb = len(matrix[0])
-    mtrx = [[row[i]**2 for row in matrix] for i in range(col_numb)]
+    mtrx = [[row[i]**2 for i in range(col_numb)] for row in matrix ]
     return mtrx
