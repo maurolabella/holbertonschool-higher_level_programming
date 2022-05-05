4109f8e129b794848a207133740f1642a55e3e33
maurolabella <maurolabella@yahoo.com>
2022-05-04 21:00:23 -0300

    a

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,20 +4,24 @@
 def roman_to_int(roman_string):
 
     if not(isinstance(roman_string, str)) or roman_string is None:
-        return None
-    romans = ["M", "D", "C", "L", "X", "V", "I"]
-    decimals = [1000, 500, 100, 50, 10, 5, 1]
-    base = list(roman_string)
-    bse_x = [int(decimals[romans.index(elem)]) for elem in roman_string]
-    max = bse_x[-1]
-    sum = 0
-    for i in reversed(range(len(bse_x))):
-        if bse_x[i] < max:
-            sum = sum - bse_x[i]
-        else:
-            max = bse_x[i]
+        return 0
+    try:
+        romans = ["M", "D", "C", "L", "X", "V", "I"]
+        decimals = [1000, 500, 100, 50, 10, 5, 1]
+        base = list(roman_string)
+        bse_x = [int(decimals[romans.index(elem)]) for elem in roman_string]
+        max = bse_x[-1]
+        sum = 0
+        for i in reversed(range(len(bse_x))):
+            if bse_x[i] < max:
+                sum = sum - bse_x[i]
+            else:
+                max = bse_x[i]
             sum = sum + bse_x[i]
-    return sum
+        return sum
+
+    except:
+        return 0
 
 # bse_sum = [(lambda x: x if (x >= max) else -x)
 #            for x in bse_x]
