633e961a968a868b412d16f7c49b1f0e28982bfa
maurolabella <maurolabella@yahoo.com>
2022-05-17 15:34:15 -0300

    A

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """Square - an empty class Square thad defines a square"""
+
+    def __init__(self, size=0):
+        """Instantiation with optional selected size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
