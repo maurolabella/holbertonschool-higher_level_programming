f9c52f8bc90ac89d642e720cf486a0722af30edc
maurolabella <maurolabella@yahoo.com>
2022-05-17 18:32:12 -0300

    C

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Square Class Creation"""
+
 
 class Square:
     """Square - an empty class Square thad defines a square"""
 
     def __init__(self, size=0):
+        """initiation"""
         self.__size = size
