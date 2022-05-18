c677217e01f62801a0c9fb4dc64f82defa5c180f
maurolabella <maurolabella@yahoo.com>
2022-05-18 18:54:00 -0300

    Z

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,8 +6,9 @@ class Rectangle:
     """define rectangle class"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
+        """set height and width"""
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -17,12 +18,11 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """width setter"""
-        if(type(value) is not int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif(value < 0):
+        if(value < 0):
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     def __str__(self):
         """String representation"""
@@ -42,17 +42,18 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """height setter"""
-        if(type(value) is not int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif(value < 0):
+        if(value < 0):
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """area retrieve function"""
         return (self.__height * self.__width)
 
     def perimeter(self):
-        """permiter retrieve function"""
+        """perimeter retrieve function"""
+        if (self.area() == 0):
+            return 0
         return ((self.__height + self.__width) * 2)
