cffac96de1ebfd838c797fdf54b4a27ae8700acd
maurolabella <maurolabella@yahoo.com>
2022-05-24 23:43:27 -0300

    a

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    i = getattr(magic_string, "value", -1)
-    setattr(magic_string, "value", i + 1)
-
+    setattr(magic_string, "value", getattr(magic_string, "value", -1) + 1)
     return "BestSchool" + ", BestSchool" * magic_string.value
