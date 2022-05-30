7b379de3403acbe70b3c941b486094fa9246360d
maurolabella <maurolabella@yahoo.com>
2022-05-30 17:32:15 -0300

    a

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry():
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Write a class BaseGeometry (based on 5-base_geometry.py)."""
+        raise Exception("area() is not implemented")
