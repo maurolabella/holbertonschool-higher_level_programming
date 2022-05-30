c6d62620f2d6d17308e576bca42ee6484f78f8f7
maurolabella <maurolabella@yahoo.com>
2022-05-30 17:08:57 -0300

    a

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Some class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """detect direct or indirect instances of a_class"""
+    return (isinstance(obj, a_class))
