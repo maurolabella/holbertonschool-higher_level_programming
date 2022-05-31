eb47b25a36792f0d9037d635ba3e875c96ecf00c
maurolabella <maurolabella@yahoo.com>
2022-05-31 19:34:48 -0300

    a

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Tasks: Create object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Write a function that creates an
+    Object from a "JSON file"""
+
+    with open(filename) as file:
+        obj = json.load(file)
+    return obj
