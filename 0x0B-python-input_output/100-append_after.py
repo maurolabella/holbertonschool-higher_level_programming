ce53276c980c0b205af9fac016a1fb08cd8881fa
maurolabella <maurolabella@yahoo.com>
2022-06-02 22:46:54 -0300

    a

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,8 +11,8 @@ def file_reader(filename):
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Write a function that inserts a line of text 
-    to a file, after each line containing 
+    Write a function that inserts a line of text
+    to a file, after each line containing
     a specific string
     """
     new_content = []
