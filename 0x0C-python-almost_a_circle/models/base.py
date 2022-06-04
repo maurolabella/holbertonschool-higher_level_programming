6a7b2fc72d11411ec605fa249fcfafa9c7d5af62
maurolabella <maurolabella@yahoo.com>
2022-06-04 16:22:21 -0300

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base Class Main File"""
 
+import json
+
 
 class Base():
     """Base"""
@@ -13,3 +15,14 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """static method that returns the JSON string
+        representation of a list_dictionaries"""
+        if type(list_dictionaries) != list:
+            raise TypeError("list_dictionaries must be a list")
+        if any(type(element) != dict for element in list_dictionaries):
+            raise TypeError("list_dicationaries must have dictionaries\
+              as elements")
+        return json.dumps(list_dictionaries)
