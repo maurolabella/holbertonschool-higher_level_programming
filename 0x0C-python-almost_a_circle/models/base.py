bce29844f376b9f5f479451c9655c87a22bc0734
maurolabella <maurolabella@yahoo.com>
2022-06-06 19:00:36 -0300

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,7 +32,7 @@ class Base():
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         else:
-            return json.dumps("[]")
+            return json.dumps([])
 
     @classmethod
     def save_to_file(cls, list_objs):
