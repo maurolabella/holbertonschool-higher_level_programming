32024e3806a93e1d43369caabb96ae6e4fdf78a4
maurolabella <maurolabella@yahoo.com>
2022-07-06 18:06:40 -0300

    a

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+--  a script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_pwd";
+GRANT ALL PRIVLEGES ON *.* TO user_0d_1@localhost;
