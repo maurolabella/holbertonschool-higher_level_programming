9b3e21b7a12afb9951fa951391ce31d74a79c66b
maurolabella <maurolabella@yahoo.com>
2022-07-06 18:44:42 -0300

    a

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- Database
 CREATE database IF NOT EXISTS hbtn_0d_2;
 -- User
-CREATE user IF	NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_02_2_pwd";
+CREATE user IF	NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_0d_2_pwd";
 -- Grant privileges
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
