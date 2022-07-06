9a75530e3a1cb32d7916390884258b6b9e6121fd
maurolabella <maurolabella@yahoo.com>
2022-07-06 20:08:41 -0300

    a

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
--- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genres.name AS genre, count(*) as number_of_shows
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the -- number of shows linked to each.
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
 FROM tv_show_genres LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
