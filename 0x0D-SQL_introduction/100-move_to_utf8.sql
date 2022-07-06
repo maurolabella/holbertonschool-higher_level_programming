7e3f04dc3e638e1e7364e8db3ae70b3ba4b6259f
maurolabella <maurolabella@yahoo.com>
2022-07-06 11:54:04 -0300

    a

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,11 @@
 --Write a script that converts hbtn_0c_0 database to UTF8
+-- Alter the database
 ALTER DATABASE hbtn_0c_0 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
+-- Specific convertion
 USE hbtn_0c_0;
+
 ALTER TABLE first_table 
 CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
