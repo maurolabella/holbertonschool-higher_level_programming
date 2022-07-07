95170560fa77d89239fa252b2d481d8b9c7ed3ff
maurolabella <maurolabella@yahoo.com>
2022-07-06 22:27:31 -0300

    a

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT title, SUM(rate) AS rating
-FROM tv_shows LEFT JOIN tv_shows_ratings
-ON tv_shows.id = tv_shows_ratings.show_id
+FROM tv_shows LEFT JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
 ORDER BY rating DESC;
