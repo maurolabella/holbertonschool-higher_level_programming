9464bef9455afe757927cc19c0c7bb75872e6623
maurolabella <maurolabella@yahoo.com>
2022-08-01 20:11:44 -0300

    a

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// executes x times a function (visible function)
+
+function callMeMoby (num, func) {
+  for (let i = 0; i < num; i++) {
+    func();
+  }
+}
+module.exports = callMeMoby;
