5d9a280f18b5c529b1b20e4d10f646bbfa65adca
maurolabella <maurolabella@yahoo.com>
2022-08-01 19:21:18 -0300

    a

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // function that returns the addition of 2 integers.
 // the function must be visible from other files "outside"
-export function add (a, b) {
-  return (Number(a) + Number(b));
+function add (x, y) {
+  return (Number(x) + Number(y));
 }
+
+module.exports = { add };
