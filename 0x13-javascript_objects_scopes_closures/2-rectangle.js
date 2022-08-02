85fd34843975e83de3e0bcd4aac6c1247e71f245
maurolabella <maurolabella@yahoo.com>
2022-08-02 17:38:20 -0300

    a

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// write a class Rectangle that defines a rectangle
+
+class Rectangle {
+  // class methods
+  constructor (w, h) {
+    // constructor takes width and height
+    if ([isNaN(w), isNaN(h), w <= 0, h <= 0].some((currentValue) => currentValue === true)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
