03614823966f141e818c5c19149b84cebb6e24a3
maurolabella <maurolabella@yahoo.com>
2022-08-04 14:21:12 -0300

    a

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,10 @@
 // Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
 const { dict } = require('./101-data');
 
-const newDict = Object.entries(dict).reduce((user, index) => {
-  !user[index[1]]
-    ? user[index[1]] = [index[0]]
-    : user[index[1]].push(index[0]);
-  return user;
+const newDict = Object.entries(dict).reduce((index, data) => {
+  !index[data[1]]
+    ? index[data[1]] = [data[0]]
+    : index[data[1]].push(data[0]);
+  return index;
 }, {});
 console.log(newDict);
---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that concatenates 2 files
+
+const fs = require('fs');
+
+const fileA = fs.readFileSync(`./${process.argv[2]}`,'utf8');
+const fileB = fs.readFileSync(`./${process.argv[3]}`,'utf8');
+const fileConcatenated = fileA + fileB;
+fs.writeFile(`${process.argv[4]}`, fileConcatenated, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
