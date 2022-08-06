89319987c590635e4ab95e4f407f7e1efaee3d37
maurolabella <maurolabella@yahoo.com>
2022-08-06 12:54:07 -0300

    a

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""SQLAlchemy ORM: Creating Session and Query on states"""
+"""SQLAlchemy ORM: Creating Session and Query on states
+   query(State).first()
+"""
 
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
+
+def main():
+    """ main function"""
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
@@ -18,6 +22,14 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        print("{:d}: {:s}".format(state.id, state.name))
+    ret = session.query(State).first()
+
+    if ret:
+        print("1: " + ret.name)
+    else:
+        print("Nothing")
+
     session.close()
+
+if __name__ == '__main__':
+      main()
