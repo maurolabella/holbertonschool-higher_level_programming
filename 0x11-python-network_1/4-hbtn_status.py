188f346c04239a999a9641f4b8d9cf0fc7753171
maurolabella <maurolabella@yahoo.com>
2022-08-19 20:01:44 -0300

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,9 @@
 
 from requests import get
 
-if __name__ == "__main__":
-    res = get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+
+    res = get('https://intranet.hbtn.io/status')
     print("Body response:")
     print(f"\t- type: {type(res.text)}")
     print(f"\t- content: {res.text}")
