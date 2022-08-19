7492f387a7e24b8dfa77a816109938299004800c
maurolabella <maurolabella@yahoo.com>
2022-08-19 17:35:32 -0300

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,17 @@ write a script that fetches https://intranet.hbtn.io/status
 
 from urllib import request
 
-with request.urlopen("https://intranet.hbtn.io/status") as webf_fetch:
-    res = webf_fetch.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(res)))
-    print(f"\t- content: {res}")
-    print(f"\t- utf8 content: {res.decode('utf-8')}")
+
+def main():
+    """ main executable"""
+    with request.urlopen("https://intranet.hbtn.io/status") as webf_fetch:
+        res = webf_fetch.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print(f"\t- content: {res}")
+        print(f"\t- utf8 content: {res.decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    """instruction to not executes when imported"""
+    main()
