810390efd80c74ea3f639d2c7f360b982f8f9bce
maurolabella <maurolabella@yahoo.com>
2022-08-19 17:31:03 -0300

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""write a script that fetches https://intranet.hbtn.io/status"""
+"""
+write a script that fetches https://intranet.hbtn.io/status
+"""
 
 from urllib import request
 
 with request.urlopen("https://intranet.hbtn.io/status") as webf_fetch:
     res = webf_fetch.read()
     print("Body response:")
-    print(f"\t- type: {type(res)}")
+    print("\t- type: {}".format(type(res)))
     print(f"\t- content: {res}")
     print(f"\t- utf8 content: {res.decode('utf-8')}")
