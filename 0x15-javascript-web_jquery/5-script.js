13c0109509a7c696d653b1373c5adff036a755ba
maurolabella <maurolabella@yahoo.com>
2022-09-19 15:49:15 -0300

    a

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,4 @@
+// Write a JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item
+$('#add_item').click(() => {
+  $('.my_list').append('<li>Item</li>');
+});
